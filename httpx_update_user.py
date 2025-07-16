6f3ddf9291783184ae8a78f5d43be7fe37ab39af
Яцуненко Артём Андреевич <yatsunenkoaa@fsk.ru>
2025-07-16 15:30:27 +0300

    Практикуемся в работе с HTTPX. Практикуемся в работе с HTTPX: обновление пользователя

---
+++ httpx_update_user.py
@@ -0,0 +1,43 @@
+import httpx
+
+from tools.fakers import get_random_email
+
+BASE_URL = "http://127.0.0.1:8000/api/v1"
+
+create_payload = {
+    "email": get_random_email(),
+    "password": "123123",
+    "lastName": "Aboba",
+    "firstName": "Artem",
+    "middleName": "JMA",
+}
+
+with httpx.Client(base_url=BASE_URL) as client:
+    creaete_response = client.post("/users", json=create_payload)
+    assert creaete_response.status_code == 200, "Ошибка создания пользователя"
+
+    uid = creaete_response.json().get("user").get("id")
+
+    login_response = client.post(
+        "/authentication/login",
+        json={
+            "email": create_payload.get("email"),
+            "password": create_payload.get("password"),
+        },
+    )
+    assert login_response.status_code == 200, "Ошибка логина"
+
+    access_token = login_response.json().get("token").get("accessToken")
+    auth_header = {"Authorization": f"Bearer {access_token}"}
+    client.headers.update(auth_header)
+
+    update_payload = {
+        "email": get_random_email(),
+        "lastName": "string",
+        "firstName": "string",
+        "middleName": "string",
+    }
+
+    update_response = client.patch(f"/users/{uid}", json=update_payload)
+    print("Status code:", update_response.status_code)
+    print("Response:", update_response.json())
---
+++ tools/fakers.py
@@ -0,0 +1,5 @@
+import time
+
+
+def get_random_email():
+    return f"example{time.time()}@mail.com"
