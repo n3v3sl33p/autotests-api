2df6e06b2e48c0da3ee85b1c21fe9eabbbd4b48e
Яцуненко Артём Андреевич <yatsunenkoaa@fsk.ru>
2025-07-22 22:58:07 +0300

    Фикстуры Pytest. Практика написания автотестов с использованием фикстур

--- tools/assertions/users.py
+++ tools/assertions/users.py
@@ -1,4 +1,9 @@
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from clients.users.users_schema import (
+    CreateUserRequestSchema,
+    CreateUserResponseSchema,
+    GetUserResponseSchema,
+    UserSchema,
+)
 from tools.assertions.base import assert_equal
 
 
@@ -16,3 +21,18 @@ def assert_create_user_response(
     assert_equal(response.user.last_name, request.last_name, "last_name")
     assert_equal(response.user.first_name, request.first_name, "first_name")
     assert_equal(response.user.middle_name, request.middle_name, "middle_name")
+
+
+def assert_users(actual: UserSchema, expected: UserSchema):
+    assert_equal(actual.id, expected.id, "id")
+    assert_equal(actual.email, expected.email, "email")
+    assert_equal(actual.last_name, expected.last_name, "last_name")
+    assert_equal(actual.first_name, expected.first_name, "first_name")
+    assert_equal(actual.middle_name, expected.middle_name, "middle_name")
+
+
+def assert_get_user_response(
+    get_user_response: GetUserResponseSchema,
+    create_user_response: CreateUserResponseSchema,
+):
+    assert_users(get_user_response.user, create_user_response.user)
